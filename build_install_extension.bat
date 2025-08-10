c9e402e192710063fb440d797bfe34f3361dfe22
Dan Bechard <dbechrd@gmail.com>
2025-08-10 11:51:51 -0400

    Ensure build_install_extension.bat always changes back to Jails dir

--- build_install_extension.bat
+++ build_install_extension.bat
@@ -3,6 +3,4 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd vscode_extension
 
-npm run compile && npm run pack:windows && code --install-extension .\jails-0.2.0.vsix
-
-cd ..
+npm run compile && npm run pack:windows && code --install-extension .\jails-0.2.0.vsix && cd ..
